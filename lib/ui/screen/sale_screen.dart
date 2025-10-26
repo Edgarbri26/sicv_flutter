e338dede882db0caf1b75a314b8893c8360ce4c9
Samuel Rosales <samuelrosales.dias@gmail.com>
2025-10-26 16:46:07 -0400

    Cambios de edgar

--- lib/ui/screen/sale_screen.dart
+++ lib/ui/screen/sale_screen.dart
@@ -1,8 +1,12 @@
 // lib/ui/pages/screen/sale_screen.dart
 
 import 'package:flutter/material.dart';
+import 'package:sicv_flutter/core/theme/app_colors.dart';
+import 'package:sicv_flutter/core/theme/app_sizes.dart';
 import 'package:sicv_flutter/models/category.dart';
 import 'package:sicv_flutter/models/product.dart';
+import 'package:sicv_flutter/ui/widgets/Info_chip.dart';
+import 'package:sicv_flutter/ui/widgets/img_product.dart';
 
 class SaleScreen extends StatefulWidget {
   final Function(Product) onProductAdded;
@@ -44,12 +48,60 @@ class _SaleScreenState extends State<SaleScreen> {
   void _loadData() {
     // Simula la carga de productos (DEBERÍAS TRAERLOS DE TU API/BD)
     _todosLosProductos = [
-      Product(id: 1, name: 'Harina PAN', description: '...', price: 1.40, stock: 50, category: Category(id: 1, name: 'Alimentos'), sku: 'ALI-001'),
-      Product(id: 2, name: 'Cigarros Marlboro', description: '...', price: 5.99, stock: 5, category: Category(id: 2, name: 'Tabaco'), sku: 'TAB-001'),
-      Product(id: 3, name: 'Café', description: '...', price: 10.99, stock: 0, category: Category(id: 3, name: 'Bebidas'), sku: 'BEB-001'),
-      Product(id: 4, name: 'Gaseosa 2L', description: '...', price: 2.5, stock: 50, category: Category(id: 3, name: 'Bebidas'), sku: 'BEB-002'),
-      Product(id: 5, name: 'Pan Campesino', description: '...', price: 2.0, stock: 15, category: Category(id: 1, name: 'Alimentos'), sku: 'ALI-002'),
-      Product(id: 6, name: 'Agua Minalba 1L', description: '...', price: 1.0, stock: 30, category: Category(id: 3, name: 'Bebidas'), sku: 'BEB-003'),
+      Product(
+        id: 1,
+        name: 'Harina PAN',
+        description: '...',
+        price: 1.40,
+        stock: 50,
+        category: Category(id: 1, name: 'Alimentos'),
+        sku: 'ALI-001',
+      ),
+      Product(
+        id: 2,
+        name: 'Cigarros Marlboro',
+        description: '...',
+        price: 5.99,
+        stock: 5,
+        category: Category(id: 2, name: 'Tabaco'),
+        sku: 'TAB-001',
+      ),
+      Product(
+        id: 3,
+        name: 'Café',
+        description: '...',
+        price: 10.99,
+        stock: 0,
+        category: Category(id: 3, name: 'Bebidas'),
+        sku: 'BEB-001',
+      ),
+      Product(
+        id: 4,
+        name: 'Gaseosa 2L',
+        description: '...',
+        price: 2.5,
+        stock: 50,
+        category: Category(id: 3, name: 'Bebidas'),
+        sku: 'BEB-002',
+      ),
+      Product(
+        id: 5,
+        name: 'Pan Campesino',
+        description: '...',
+        price: 2.0,
+        stock: 15,
+        category: Category(id: 1, name: 'Alimentos'),
+        sku: 'ALI-002',
+      ),
+      Product(
+        id: 6,
+        name: 'Agua Minalba 1L',
+        description: '...',
+        price: 1.0,
+        stock: 30,
+        category: Category(id: 3, name: 'Bebidas'),
+        sku: 'BEB-003',
+      ),
     ];
 
     // Simula la carga de categorías (DEBERÍAS TRAERLAS DE TU API/BD)
@@ -79,9 +131,11 @@ class _SaleScreenState extends State<SaleScreen> {
     // 2. Filtrar por texto de búsqueda
     if (searchText.isNotEmpty) {
       results = results
-          .where((product) =>
-              product.name.toLowerCase().contains(searchText) ||
-              (product.sku ?? '').toLowerCase().contains(searchText)) // Busca por nombre o SKU
+          .where(
+            (product) =>
+                product.name.toLowerCase().contains(searchText) ||
+                (product.sku ?? '').toLowerCase().contains(searchText),
+          ) // Busca por nombre o SKU
           .toList();
     }
 
@@ -103,24 +157,36 @@ class _SaleScreenState extends State<SaleScreen> {
           child: TextField(
             controller: _searchController,
             decoration: InputDecoration(
-              labelText: 'Buscar producto por nombre o SKU',
+              filled: true,
+              fillColor: AppColors.secondary,
+              labelText: 'Buscar por Nombre o SKU',
               prefixIcon: Icon(Icons.search),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12.0),
+              labelStyle: TextStyle(
+                fontSize: 14.0, // <-- Cambia el tamaño de la fuente del label
+                color: AppColors.textSecondary, // (Opcional: define el color del label)
               ),
-              // Añade un botón para limpiar el texto
-              suffixIcon: _searchController.text.isNotEmpty
-                  ? IconButton(
-                      icon: Icon(Icons.clear),
-                      onPressed: () {
-                        _searchController.clear();
-                      },
-                    )
-                  : null,
+
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: BorderSide(
+                  width: 2.0, // <-- Tu grosor deseado
+                  color: AppColors.border, // Color del borde
+                ),
+              ),
+
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide: BorderSide(
+                    width: 3.0, // <-- Puedes poner un grosor mayor al enfocar
+                    color: AppColors.textSecondary, // Color del borde al enfocar
+                ),
+              ),
+              
+              contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 16),
             ),
           ),
         ),
-        
+
         // --- 2. WIDGET DE FILTRO DE CATEGORÍAS ---
         _buildCategoryFilter(),
 
@@ -138,15 +204,25 @@ class _SaleScreenState extends State<SaleScreen> {
                     childAspectRatio: 0.7, // Ajusta la altura (Ancho / Alto)
                   ),
                   // --- FIN DE MEJORA ---
-                  itemCount: _productosFiltrados.length, // Usa la lista filtrada
+                  itemCount:
+                      _productosFiltrados.length, // Usa la lista filtrada
                   itemBuilder: (context, index) {
-                    final product = _productosFiltrados[index]; // Usa la lista filtrada
+                    final product =
+                        _productosFiltrados[index]; // Usa la lista filtrada
                     bool isOutOfStock = product.stock == 0;
 
-                    return Card(
+                    return Container(
                       clipBehavior: Clip.antiAlias,
-                      elevation: 2,
-                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                      decoration: BoxDecoration(
+                        color: AppColors.secondary,
+                        borderRadius: BorderRadius.circular(
+                          AppSizes.borderRadiusL,
+                        ),
+                        border: Border.all(
+                          color: AppColors.border,
+                          width: 2,
+                        ),
+                      ),
                       child: InkWell(
                         onTap: isOutOfStock
                             ? null
@@ -158,47 +234,58 @@ class _SaleScreenState extends State<SaleScreen> {
                             children: [
                               Expanded(
                                 flex: 3,
-                                child: Container(
-                                  color: Colors.grey.shade200,
-                                  child: (product.imageUrl != null && product.imageUrl!.isNotEmpty)
-                                      ? Image.network(product.imageUrl!, fit: BoxFit.cover)
-                                      : Icon(Icons.inventory_2, size: 40, color: Colors.grey.shade400),
+                                child: ImgProduct(
+                                  imageUrl: product.imageUrl ?? '',
                                 ),
                               ),
                               Expanded(
                                 flex: 2,
                                 child: Padding(
                                   padding: const EdgeInsets.all(8.0),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        product.name,
-                                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                                        maxLines: 2,
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                                      Spacer(),
-                                      Text(
-                                        '\$${product.price.toStringAsFixed(2)}',
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          color: Theme.of(context).primaryColor,
-                                          fontSize: 15,
-                                        ),
-                                      ),
-                                      // --- MEJORA DE UI (STOCK) ---
-                                      if (isOutOfStock)
-                                        Text(
-                                          'Agotado',
-                                          style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold, fontSize: 10),
-                                        )
-                                      else if (product.stock <= 10) // Muestra si queda poco stock
-                                        Text(
-                                          'Quedan: ${product.stock}',
-                                          style: TextStyle(color: Colors.orange.shade800, fontWeight: FontWeight.bold, fontSize: 10),
-                                        ),
-                                    ],
+                                  child: LayoutBuilder(
+                                    builder: (context, constraints) {
+                                      final ancho = constraints.maxWidth;
+                                      return Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            product.name,
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize:
+                                                  ancho *
+                                                  0.12, // Puedes ajustar el factor
+                                            ),
+                                            maxLines: 2,
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                          Spacer(),
+                                          Row(
+                                            spacing: AppSizes.spacingXXS,
+                                            children: [
+                                              InfoChip(
+                                                text:
+                                                    '\$${product.price.toStringAsFixed(2)}',
+                                                color: AppColors.info,
+                                              ),
+                                              if (product.stock > 0)
+                                                InfoChip(
+                                                  text: '${product.stock} Uds.',
+                                                  color: product.stock > 5
+                                                      ? AppColors.success
+                                                      : AppColors.edit,
+                                                )
+                                              else
+                                                InfoChip(
+                                                  text: 'Agotado',
+                                                  color: AppColors.error,
+                                                ),
+                                            ],
+                                          ),
+                                        ],
+                                      );
+                                    },
                                   ),
                                 ),
                               ),
@@ -207,6 +294,97 @@ class _SaleScreenState extends State<SaleScreen> {
                         ),
                       ),
                     );
+                    // return Card(
+                    //   clipBehavior: Clip.antiAlias,
+                    //   // color: AppColors.disabled,
+                    //   elevation: 0,
+                    //   shape: RoundedRectangleBorder(
+                    //     borderRadius: BorderRadius.circular(
+                    //       AppSizes.borderRadiusM,
+                    //     ),
+                    //   ),
+                    //   child: InkWell(
+                    //     onTap: isOutOfStock
+                    //         ? null
+                    //         : () => widget.onProductAdded(product),
+                    //     child: Opacity(
+                    //       opacity: isOutOfStock ? 0.5 : 1.0,
+                    //       child: Column(
+                    //         crossAxisAlignment: CrossAxisAlignment.stretch,
+                    //         children: [
+                    //           Expanded(
+                    //             flex: 3,
+                    //             child: Container(
+                    //               color: Colors.grey.shade200,
+                    //               child:
+                    //                   (product.imageUrl != null &&
+                    //                       product.imageUrl!.isNotEmpty)
+                    //                   ? Image.network(
+                    //                       product.imageUrl!,
+                    //                       fit: BoxFit.cover,
+                    //                     )
+                    //                   : Icon(
+                    //                       Icons.inventory_2,
+                    //                       size: 40,
+                    //                       color: Colors.grey.shade400,
+                    //                     ),
+                    //             ),
+                    //           ),
+                    //           Expanded(
+                    //             flex: 2,
+                    //             child: Padding(
+                    //               padding: const EdgeInsets.all(8.0),
+                    //               child: Column(
+                    //                 crossAxisAlignment:
+                    //                     CrossAxisAlignment.start,
+                    //                 children: [
+                    //                   Text(
+                    //                     product.name,
+                    //                     style: TextStyle(
+                    //                       fontWeight: FontWeight.bold,
+                    //                       fontSize: 16,
+                    //                     ),
+                    //                     maxLines: 2,
+                    //                     overflow: TextOverflow.ellipsis,
+                    //                   ),
+                    //                   Spacer(),
+                    //                   Text(
+                    //                     '\$${product.price.toStringAsFixed(2)}',
+                    //                     style: TextStyle(
+                    //                       fontWeight: FontWeight.bold,
+                    //                       color: Theme.of(context).primaryColor,
+                    //                       fontSize: 15,
+                    //                     ),
+                    //                   ),
+                    //                   // --- MEJORA DE UI (STOCK) ---
+                    //                   if (isOutOfStock)
+                    //                     Text(
+                    //                       'Agotado',
+                    //                       style: TextStyle(
+                    //                         color: Colors.red,
+                    //                         fontWeight: FontWeight.bold,
+                    //                         fontSize: 10,
+                    //                       ),
+                    //                     )
+                    //                   else if (product.stock <=
+                    //                       10) // Muestra si queda poco stock
+                    //                     Text(
+                    //                       'Quedan: ${product.stock}',
+                    //                       style: TextStyle(
+                    //                         color: Colors.orange.shade800,
+                    //                         fontWeight: FontWeight.bold,
+                    //                         fontSize: 10,
+                    //                       ),
+                    //                     ),
+                    //                 ],
+                    //               ),
+                    //             ),
+                    //           ),
+                    //         ],
+                    //       ),
+                    //     ),
+                    //   ),
+                    // );
                   },
                 ),
         ),
@@ -243,10 +421,12 @@ class _SaleScreenState extends State<SaleScreen> {
               labelStyle: TextStyle(
                 color: isSelected ? Colors.white : Colors.black,
               ),
-              backgroundColor: Colors.grey.shade200,
+              backgroundColor: AppColors.secondary,
               shape: StadiumBorder(
                 side: BorderSide(
-                  color: isSelected ? Theme.of(context).primaryColor : Colors.grey.shade300,
+                  color: isSelected
+                      ? Theme.of(context).primaryColor
+                      : Colors.grey.shade300,
                 ),
               ),
             ),
---
+++ lib/ui/widgets/img_product.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:sicv_flutter/core/theme/app_colors.dart';
+import 'package:sicv_flutter/core/theme/app_sizes.dart';
+
+class ImgProduct extends StatelessWidget {
+  const ImgProduct({
+    super.key,
+    required this.imageUrl,
+    this.width,
+    this.height,
+  });
+  final String imageUrl;
+  final double? width;
+  final double? height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.antiAlias,
+      decoration: BoxDecoration(
+        color: AppColors.iconPassive.withValues(alpha: 0.2),
+        borderRadius: BorderRadius.circular(AppSizes.borderRadiusM),
+      ),
+      child: (imageUrl.isNotEmpty)
+          ? Image.network(
+              imageUrl,
+              fit: BoxFit.cover,
+              width: width,
+              height: height,
+            )
+          : Icon(Icons.inventory_2, size: 40, color: AppColors.primary),
+    );
+  }
+}
